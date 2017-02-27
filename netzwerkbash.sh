c60b1cbd75a9aea46df8acf0eb13c6902e5d1f3c
Hanner72 <johann.danner@gmail.com>
2017-02-27 09:37:59 +0100

    Create netzwerkbash.sh

---
+++ netzwerkbash.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#############################################
+#          Created by Johann Danner         #
+#    E-Mail: johann.danner(at)gmail.com     #
+#   Feel free to copy & share this script   #
+#############################################
+
+# the IP of your raspberry
+raspi_client_ip="192.168.2.2"
+# subnetmask
+raspi_client_nm="255.255.255.0"
+# the IP of your router
+raspi_client_gw="192.168.2.1"
